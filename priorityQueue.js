e775cccf687940439c16bf0b194e36f3d9a4ad23
Sandro Scheffmann <Sandro@saromi-no-MacBook-Pro.local>
2020-05-03 17:26:06 +0900

    priority queue setup

---
+++ priorityQueue.js
@@ -0,0 +1,32 @@
+class PriorityQueue {
+  #items = [];
+
+  get count() {
+    return this.#items.length;
+  }
+
+  enqueue(item) {
+    this.#items = [...this.#items, item];
+  }
+
+  dequeue() {
+    return this.count ? this.#items.splice(0, 1)[0] : undefined;
+  }
+
+  first() {
+    return this.count ? this.#items[0] : undefined;
+  }
+}
+
+const queue = new PriorityQueue();
+
+
+queue.enqueue('a');
+queue.enqueue('t');
+queue.enqueue('s');
+
+console.log(queue.count);
+console.log(queue.first());
+console.log(queue.dequeue())
+console.log(queue.count);
+console.log(queue.first());
